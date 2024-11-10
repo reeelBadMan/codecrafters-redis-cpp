f30ef983e75b0f8b91d1e77e8122553c30f8e938
ReelBadMan <ciecuits@gmail.com>
2024-11-10 08:57:38 +0100

    codecrafters submit [skip ci]

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "server/Server.h"
+
+int main() {
+    Server server(6379);
+
+    if (!server.initialize()) {
+        std::cerr << "Server initialization failed\n";
+        return 1;
+    }
+
+    server.waitForConnection();
+
+    return 0;
+}
